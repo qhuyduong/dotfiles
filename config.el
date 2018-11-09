e4c8ffbed0a47802516b344c32b7697b00aa8d63
Huy Duong <huy.duong@employmenthero.com>
2018-11-09 20:40:19 +0700

    Add some configs
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- config.el
+++ config.el
@@ -1,3 +1,13 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Emacs GUI settings
+(setq doom-font (font-spec :family "Monaco" :size 14))
+(setq doom-theme 'doom-city-lights)
+;; Make titlebar match background color
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
+
+;; Reduce which-key delay
+(setq which-key-idle-delay 0.5)
+
