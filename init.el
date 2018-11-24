b1e1d657a0775408359e8ce3da1436b8716b50fb
Huy Duong <huy.duong@employmenthero.com>
2018-11-24 23:55:39 +0700

    Remove pretty-code module
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- init.el
+++ init.el
@@ -30,7 +30,6 @@
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
-       pretty-code       ; replace bits of code with pretty symbols
        treemacs          ; a project drawer, like neotree but cooler
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
