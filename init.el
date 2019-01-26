050fdde7b2322a507ebb1f5e90175a46bc0c7f4f
Huy Duong <huy.duong@employmenthero.com>
2019-01-26 12:58:45 +0700

    Remove doom-dashboard and doom-quit
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- init.el
+++ init.el
@@ -22,8 +22,6 @@
 
        :ui
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
        evil-goggles      ; display visual hints when editing in evil
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        modeline          ; snazzy, Atom-inspired modeline, plus API
