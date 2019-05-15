295c088e78978d89dbc2d52887b7127356c6340c
Huy Duong <huy.duong@employmenthero.com>
2019-05-15 15:19:23 +0700

    Use box for tabbar-selected face
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- config.el
+++ config.el
@@ -347,7 +347,7 @@
   (setq tabbar-background-color "#3e4151")
   (setq tabbar-buffer-list-function #'+tabbar-buffer-list)
   (setq tabbar-buffer-groups-function #'+tabbar-buffer-groups)
-  (custom-set-faces '(tabbar-selected ((t (:background "#292d3e" :slant italic :weight medium))))
+  (custom-set-faces '(tabbar-selected ((t (:background "#292d3e" :slant italic :weight medium :box 1))))
                     '(tabbar-selected-modified ((t (:foreground "#ff5370"))))
                     '(tabbar-unselected ((t (:foreground "#EEFFFF" :background "#3e4151"))))
                     '(tabbar-unselected-modified ((t (:background "#3e4151")))))
