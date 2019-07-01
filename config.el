db1cfa4e366b607ae73c6f38dc08fc8a10eab283
Huy Duong <huy.duong@employmenthero.com>
2019-07-01 21:54:12 +0700

    Revert "Use default icons for treemacs"
    This reverts commit cfd2c50fe345f172c8e655c07e7f8fc782ce8843.

--- config.el
+++ config.el
@@ -226,7 +226,6 @@
   (setq rspec-use-rvm nil))
 
 (after! treemacs
-  (treemacs-load-theme "Default")
   (map! :mode treemacs-mode
         "C-h"  #'evil-window-left
         "C-j"  #'evil-window-down
