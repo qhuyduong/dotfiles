4d0d9d179de588e276095f151f57c624dae0624d
Huy Duong <huy.duong@employmenthero.com>
2019-08-23 11:38:12 +0700

    Add move-line-up and move-line-down
    How the hell these functions got removed!
    
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- config.el
+++ config.el
@@ -673,3 +673,14 @@ T - tag prefix
   "Navigate to the right tmux pane"
   (interactive)
   (tmux-navigate "right"))
+
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
