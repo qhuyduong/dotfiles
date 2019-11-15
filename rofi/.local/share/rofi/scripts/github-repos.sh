dfe1ece9931dc05bad266f48a9fda7f5585817ea
Huy Duong <huy.duong@employmenthero.com>
2019-11-15 16:51:32 +0700

    [rofi] Update github script
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- rofi/.local/share/rofi/scripts/github-repos.sh
+++ rofi/.local/share/rofi/scripts/github-repos.sh
@@ -4,7 +4,7 @@ CACHE_FILE=~/.local/share/rofi/scripts/cache/github-repos.cache
 
 # Get all the repositories for the user with curl and GitHub API
 fetch_repositories() {
-  echo > $CACHE_FILE
+  touch $CACHE_FILE
   for INDEX in 1 2 3; do
     curl -s -H "authorization: token $GITHUB_TOKEN" "https://api.github.com/user/repos?per_page=100&page=$INDEX" | jq -r 'map(.full_name) | join("\n")' >> $CACHE_FILE
   done
