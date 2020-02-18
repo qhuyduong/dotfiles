404b765dabb072900e977c94a7a04e73f7908894
Huy Duong <huy.duong@employmenthero.com>
2020-02-18 12:02:37 +0700

    [rofi] Update github repo fetching script
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- rofi/.local/share/rofi/scripts/github-repos.sh
+++ rofi/.local/share/rofi/scripts/github-repos.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 CACHE_FILE=~/.local/share/rofi/scripts/cache/github-repos.cache
 
 # Get all the repositories for the user with curl and GitHub API
 fetch_repositories() {
-  echo > $CACHE_FILE
-  for INDEX in 1 2 3; do
+  PAGES=$(curl -s -I -H "authorization: token $GITHUB_TOKEN" "https://api.github.com/user/repos?per_page=100&page=1" | sed -r -n '/^Link:/ s/.*page=([0-9]+).*/\1/p')
+  for INDEX in {1..$PAGES}; do
     curl -s -H "authorization: token $GITHUB_TOKEN" "https://api.github.com/user/repos?per_page=100&page=$INDEX" | jq -r 'map(.full_name) | join("\n")' >> $CACHE_FILE
   done
 }
 
 main() {
+  echo > $CACHE_FILE
   fetch_repositories
 }
 
