253864be80ea61826c5e92be7ed92f9fb4f252f7
Huy Duong <huy.duong@employmenthero.com>
2020-03-10 07:35:14 +0700

    Remove unused hydra
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- config.el
+++ config.el
@@ -356,14 +356,7 @@
 (use-package! monkeyc-mode
   :mode "\\.mc\\'")
 
-;;;;;;;;;; Hydras ;;;;;;;;;;
-
-(defhydra evil-window-resize-hydra (:hint nil)
-  ("k" (evil-window-increase-height 10) "increase height by 10 rows")
-  ("j" (evil-window-decrease-height 10) "decrease height by 10 rows")
-  ("l" (evil-window-increase-width 10) "increase width by 10 columns")
-  ("h" (evil-window-decrease-width 10) "decrease width by 10 columns")
-  ("q" nil "quit"))
+;;;;;;;;;; Functions ;;;;;;;;;;
 
 (defun projectile-frontend-core-related-files (path)
   (when (string-match "\\(.*\\)\/\\(.*\\)$" path)
@@ -373,57 +366,6 @@
       (if (projectile-test-file-p file-name)
           (list :impl (concat dir "/../" base-file-name ".js"))
         (list :test (concat dir "/__tests__/" base-file-name ".spec.js"))))))
-
-(defhydra +dired-hydra (:hint nil :color pink)
-  "
-_+_ mkdir          _v_iew           _m_ark             _(_ details        _i_nsert-subdir    wdired
-_C_opy             _O_ view other   _U_nmark all       _)_ omit-mode      _$_ hide-subdir    C-x C-q : edit
-_D_elete           _o_pen other     _u_nmark           _l_ redisplay      _w_ kill-subdir    C-c C-c : commit
-_R_ename           _M_ chmod        _t_oggle           _g_ revert buf     _e_ ediff          C-c ESC : abort
-_Y_ rel symlink    _G_ chgrp        _E_xtension mark   _s_ort             _=_ pdiff
-_S_ymlink          ^ ^              _F_ind marked      _._ toggle hydra   \\ flyspell
-_r_sync            ^ ^              ^ ^                ^ ^                _?_ summary
-_z_ compress-file  _A_ find regexp
-_Z_ compress       _Q_ repl regexp
-
-T - tag prefix
-"
-  ("\\" dired-do-ispell)
-  ("(" dired-hide-details-mode)
-  (")" dired-omit-mode)
-  ("+" dired-create-directory)
-  ("=" diredp-ediff)         ;; smart diff
-  ("?" dired-summary)
-  ("$" diredp-hide-subdir-nomove)
-  ("A" dired-do-find-regexp)
-  ("C" dired-do-copy)        ;; Copy all marked files
-  ("D" dired-do-delete)
-  ("E" dired-mark-extension)
-  ("e" dired-ediff-files)
-  ("F" dired-do-find-marked-files)
-  ("G" dired-do-chgrp)
-  ("g" revert-buffer)        ;; read all directories again (refresh)
-  ("i" dired-maybe-insert-subdir)
-  ("l" dired-do-redisplay)   ;; relist the marked or singel directory
-  ("M" dired-do-chmod)
-  ("m" dired-mark)
-  ("O" dired-display-file)
-  ("o" dired-find-file-other-window)
-  ("Q" dired-do-find-regexp-and-replace)
-  ("R" dired-do-rename)
-  ("r" dired-do-rsynch)
-  ("S" dired-do-symlink)
-  ("s" dired-sort-toggle-or-edit)
-  ("t" dired-toggle-marks)
-  ("U" dired-unmark-all-marks)
-  ("u" dired-unmark)
-  ("v" dired-view-file)      ;; q to exit, s to search, = gets line #
-  ("w" dired-kill-subdir)
-  ("Y" dired-do-relsymlink)
-  ("z" diredp-compress-this-file)
-  ("Z" dired-do-compress)
-  ("q" nil)
-  ("." nil :color blue))
 
 (dolist (i (number-sequence 0 9))
   (eval `(defun ,(intern (format "+workspace-switch-to-%s" i)) nil
@@ -461,8 +403,6 @@ T - tag prefix
   +workspace-hydra/hint)
 
 (advice-add '+workspace--tabline :override #'+workspace--tabline-hydra)
-
-;;;;;;;;;; Functions ;;;;;;;;;;
 
 (defun move-line-up ()
   (interactive)
