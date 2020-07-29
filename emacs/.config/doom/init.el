6977986f9e41c9c2b103e7167470adaa3dbb2974
Huy Duong <huy.duong@employmenthero.com>
2020-07-29 14:53:17 +0700

    Use undo-tree
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -47,7 +47,8 @@
         +icons)          ; colorful icons for dired-mode
        electric          ; smarter, keyword-based electric-indent
        ibuffer           ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
+       (undo             ; persistent, smarter undo for your inevitable mistakes
+        +tree)
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
