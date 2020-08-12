875b979072830d2f186b81156b273a56812e6b43
Huy Duong <huy.duong@employmenthero.com>
2020-08-12 21:09:41 +0700

    Disable light modeline
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -25,8 +25,7 @@
        doom-dashboard    ; a nifty splash screen for Emacs
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
-       (modeline         ; snazzy, Atom-inspired modeline, plus API
-        +light)
+       modeline          ; snazzy, Atom-inspired modeline, plus API
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
