5f7fc012d7d30eba39d797de7bf35bc5ba9e6d6b
Huy Duong <huy.duong@employmenthero.com>
2020-10-05 11:00:21 +0700

    Enable aspell
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -55,7 +55,9 @@
        :checkers
        (syntax           ; tasing you for every semicolon you forget
         +childframe)
-       spell             ; tasing you for misspelling mispelling
+       (spell            ; tasing you for misspelling mispelling
+        +aspell
+        +everywhere)
        grammar           ; tasing grammar mistake every you make
 
        :tools
