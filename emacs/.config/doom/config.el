c6134132c82a0b89e8d4e807e1182c2660fd519c
Huy Duong <huy.duong@employmenthero.com>
2020-10-26 22:16:10 +0700

    Revert to doom-palenight theme
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -1,7 +1,8 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Global settings
-(setq doom-localleader-key ","
+(setq doom-theme 'doom-palenight
+      doom-localleader-key ","
       display-line-numbers-type nil
       initial-scratch-message (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n")
       doom-font (font-spec :family "Fira Code" :size 16)
