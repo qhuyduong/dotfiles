b6aff601b55d70c743bd7b9502e936431092f96c
Huy Duong <huy.duong@employmenthero.com>
2020-10-27 14:59:08 +0700

    Change local leader key
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -2,7 +2,7 @@
 
 ;; Global settings
 (setq doom-theme 'doom-palenight
-      doom-localleader-key ","
+      doom-localleader-key "'"
       display-line-numbers-type 'relative
       doom-font (font-spec :family "Fira Code" :size 16)
       default-input-method "vietnamese-telex"
