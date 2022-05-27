57488c68db43777b28542a650b8f9d4ea77648b0
Huy Duong <huy.duong@employmenthero.com>
2022-05-27 15:49:23 +0700

    [nvim] Minor updates

--- nvim/.config/nvim/lua/custom/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -5,7 +5,7 @@ local sources = {
 
    -- webdev stuff
    b.formatting.deno_fmt,
-   b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "json" } },
+   b.formatting.prettier,
 
    -- Lua
    b.formatting.stylua,
