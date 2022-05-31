1e9819b2e5cd1a53024019488eb4b67d029dfba4
Huy Duong <huy.duong@employmenthero.com>
2022-05-31 16:19:58 +0700

    [nvim] Better null-ls format

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -10,7 +10,7 @@ return {
    ["jose-elias-alvarez/null-ls.nvim"] = {
       after = "nvim-lspconfig",
       config = function()
-         require("custom.plugins.null-ls").setup()
+         require "custom.plugins.null-ls"
       end,
    },
 
--- nvim/.config/nvim/lua/custom/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -2,38 +2,57 @@ local null_ls = require "null-ls"
 local b = null_ls.builtins
 
 local sources = {
-
-   -- webdev stuff
-   b.formatting.deno_fmt,
    b.formatting.prettier,
-
-   -- Lua
    b.formatting.stylua,
-
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
-
-   -- cpp
-   b.formatting.clang_format,
-
-   -- Ruby
    b.formatting.rubocop,
 }
 
-local M = {}
+local async_formatting = function(bufnr)
+   bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-M.setup = function()
-   null_ls.setup {
-      debug = true,
-      sources = sources,
-      -- format on save
-      on_attach = function(client)
-         if client.resolved_capabilities.document_formatting then
-            vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+   vim.lsp.buf_request(
+      bufnr,
+      "textDocument/formatting",
+      { textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
+      function(err, res, ctx)
+         if err then
+            local err_msg = type(err) == "string" and err or err.message
+            -- you can modify the log message / level (or ignore it completely)
+            vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+            return
          end
-      end,
-   }
+
+         -- don't apply results if buffer is unloaded or has been modified
+         if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+            return
+         end
+
+         if res then
+            local client = vim.lsp.get_client_by_id(ctx.client_id)
+            vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+            vim.api.nvim_buf_call(bufnr, function()
+               vim.cmd "silent noautocmd update"
+            end)
+         end
+      end
+   )
 end
 
-return M
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup {
+   -- add your sources / config options here
+   sources = sources,
+   on_attach = function(client, bufnr)
+      if client.supports_method "textDocument/formatting" then
+         vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+         vim.api.nvim_create_autocmd("BufWritePost", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+               async_formatting(bufnr)
+            end,
+         })
+      end
+   end,
+}
