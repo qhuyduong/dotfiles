1d8ba98f7777ac7fcc5854cbf48912f9fe6acdb5
Huy Duong <qhuyduong@hotmail.com>
2023-03-30 18:14:51 +0700

    Change statusline theme

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -2,19 +2,22 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require("custom.highlights")
 
 M.ui = {
-  theme = "tokyonight",
-  theme_toggle = { "tokyonight", "one_light" },
+	theme = "tokyonight",
+	theme_toggle = { "tokyonight", "one_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
+	statusline = {
+		theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+	},
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
