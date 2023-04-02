1fc4f89b2c5c4b5b2c35b0e9b1386819564c02d4
Huy Duong <qhuyduong@hotmail.com>
2023-04-03 02:57:12 +0700

    [nvim] Update theme
    Signed-off-by: Huy Duong <qhuyduong@hotmail.com>

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-	theme = "tokyonight",
-	theme_toggle = { "tokyonight", "one_light" },
+	theme = "catppuccin",
+	theme_toggle = { "catppuccin", "one_light" },
 
 	hl_override = highlights.override,
 	hl_add = highlights.add,
