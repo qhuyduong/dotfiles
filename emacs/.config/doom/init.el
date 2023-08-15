ab2c357a4b5ba8cc909d09dd4e8d9143af259c16
Huy Duong <qhuyduong92@gmail.com>
2023-08-15 13:57:31 +0700

    [emacs] Enable LSP for python

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -162,7 +162,9 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python            ; beautiful is better than ugly
+        +lsp
+        +tree-sitter)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
