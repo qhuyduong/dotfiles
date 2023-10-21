fb5bdffcd2ae0df12ccb37f53089fa778e971077
Huy Duong <qhuyduong92@gmail.com>
2023-10-21 19:55:58 +0700

    Add utilities

---
+++ utilities/lowbat.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+acpi -b | grep "Battery 0" | awk -F'[,:%]' '{print $2, $3}' | {
+	read -r status capacity
+
+	if [ "$status" = Discharging -a "$capacity" -lt 5 ]; then
+		logger "Critical battery threshold"
+		systemctl hibernate
+	fi
+}
