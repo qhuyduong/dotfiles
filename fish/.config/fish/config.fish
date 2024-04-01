9f637bd06cf7b5be64073800c377d55aedb39b50
Huy Duong <quochuy.duong@motorolasolutions.com>
2024-04-01 23:08:33 +0800

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
-    pyenv init - | source
     jump shell fish | source
 
     if not set -q TMUX; and [ "$TERM_PROGRAM" != "vscode" ]
         tmux new-session -A -s main
     end
 end
+set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0 #GWSL
+set -gx PULSE_SERVER tcp:(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}') #GWSL
+set -gx LIBGL_ALWAYS_INDIRECT 1 #GWSL
