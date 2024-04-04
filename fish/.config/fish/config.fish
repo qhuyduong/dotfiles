e34cd5dd29b33e9c995e4ad7ab26757973819aca
Huy Duong <quochuy.duong@motorolasolutions.com>
2024-04-04 10:33:29 +0800

    [wsl] Disable LIBGL_ALWAYS_INDIRECT

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,4 +7,3 @@ if status is-interactive
 end
 set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0 #GWSL
 set -gx PULSE_SERVER tcp:(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}') #GWSL
-set -gx LIBGL_ALWAYS_INDIRECT 1 #GWSL
