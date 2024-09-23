46c2973bee0248c1f11b6a45aefdc8dec4660cf9
Huy Duong <huy.duong@ologn.tech>
2024-09-23 09:24:42 +0700

    [fish] Add pyenv and rbenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,13 @@
 if status is-interactive
     jump shell fish | source
+    pyenv init - | source
+    rbenv init - --no-rehash fish | source
 
     if not set -q TMUX; and [ "$TERM_PROGRAM" != "vscode" ]
         tmux new-session -A -s main
     end
 end
+
 set -gx DISPLAY (ip route list default | awk '{print $3}'):0.0 # GWSL
 set -gx PULSE_SERVER tcp:(ip route list default | awk '{print $3}') # GWSL
 set -gx LIBGL_ALWAYS_INDIRECT 1 # GWSL
