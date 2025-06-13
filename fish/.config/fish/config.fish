9edd06c6129aa70a090f3941c56bd5b073a1cf01
Huy Duong <huy.duong@ologn.tech>
2025-06-13 13:16:10 +0000

    [fish] Add rbenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     jump shell fish | source
+    ~/.rbenv/bin/rbenv init - --no-rehash fish | source
 
     if not set -q TMUX; and [ "$TERM_PROGRAM" != "vscode" ]
         tmux new-session -A -s main
